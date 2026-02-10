18d5d073d03cf28186bfb81a760ec3b0049be958
Robert <robert32176@outlook.com>
2026-02-10 15:12:43 +0200

    v1

---
+++ src/main/java/com/example/demo/Model/User.java
@@ -0,0 +1,12 @@
+package com.example.demo.Model;
+
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password=password;
+    }
+}
+
