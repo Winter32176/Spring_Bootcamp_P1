481818b75a528815f3585b571bf1ff8f44db4048
Robert <robert32176@outlook.com>
2026-02-11 10:30:20 +0200

    TasM16

--- src/main/java/com/example/demo/Model/MotorDAO.java
+++ src/main/java/com/example/demo/Model/MotorDAO.java
@@ -9,4 +9,25 @@ public class MotorDAO {
     private Long id;
 
     public String type;
+
+    public MotorDAO(Long id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
